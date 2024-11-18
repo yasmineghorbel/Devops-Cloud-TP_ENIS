d4329f12b41f7a6c38e567d472e015d6c6ebbfb6
imenchaari2 <imen.chaari@enis.tn>
2024-11-18 18:21:59 +0100

    update jenkinsfile and terraform main

--- my-terraform-project/outputs.tf
+++ my-terraform-project/outputs.tf
@@ -9,6 +9,11 @@ output "instance_details" {
     subnet_id            = aws_instance.public_instance.subnet_id
   }
 }
+output "deployer_key_s3_uri" {
+  description = "S3 URI of the deployer key file"
+  value       = "s3://${aws_s3_object.private_key_object.bucket}/${aws_s3_object.private_key_object.key}"
+}
+
 output "rds_endpoint" {
   description = "The endpoint of the RDS database"
   value = {
